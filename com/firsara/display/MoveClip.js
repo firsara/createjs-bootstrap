2d661b125715da8c4bd4b151d7f6de350785e10f
Fabian Irsara <info@fabianirsara.com>
2014-09-10 19:01:42 +0200

    adjust to make use of transformable base class

--- com/firsara/display/MoveClip.js
+++ com/firsara/display/MoveClip.js
@@ -9,45 +9,43 @@
  * element.borders.y = [0, stage.canvas.height];
  */
 
+// TODO: dispatch better events!
+// TODO: snap, lock, free each specific to transformed value
+
 setPackage('com.firsara.display');
 
 com.firsara.display.MoveClip = (function(){
-  var Parent = createjs.Container;
+  var Parent = com.firsara.display.Transformable;
 
-  // constants
-  var STACK_MOVE = 4;
+  var MOVE = 'move';
+  var MOVE_COMPLETE = 'moveComplete';
 
   var MoveClip = function(){
     // reference to instance
     var self = this;
 
+    // layer active moveclip to top
+    self.level = false;
+
     // numeric value to snap movement to x pixels
     self.snap = null;
     // moveable free around entire stage, overwrites borders if true
     self.free = false;
-    // layer active moveclip to top
-    self.level = false;
-    // lock movement regardless of other values
-    self.lock = false;
-    // constraint movement to [-x, x] / [-y, y]
-    self.borders = {x: [], y: []};
-    // lower / higher movement through frictions
-    self.friction = {move: 1, release: 1};
 
-    // private variables
-    var _store = {
-      oldPosition: {x: 0, y: 0},
-      position: {x: 0, y: 0},
-      move: [],
-      tween: null
-    };
+    // stored fade-out-tween
+    var _tween = null;
 
     // constructor
     var Init = function(){
-      self.addEventListener('mousedown', startDrag);
-      self.addEventListener('pressmove', pressMove);
-      self.addEventListener('pressup', fadeMove);
-      self.addEventListener('tick', storeMove);
+      // call super constructor, only if instance is not a mixin of another class
+      if (Parent && ! self.borders) Parent.call(self);
+
+      self.borders.x = [];
+      self.borders.y = [];
+
+      self.addEventListener('start', _startTransform);
+      self.addEventListener('update', _update);
+      self.addEventListener('pressup', _stopTransform);
     };
 
     self.moveTo = function(x, y, ease){
@@ -59,15 +57,34 @@ com.firsara.display.MoveClip = (function(){
         if (! isNaN(y)) options.y = y;
         if (ease) options.ease = ease;
         else options.ease = Cubic.easeOut;
-        options.onComplete = dispatchComplete;
+        options.onComplete = _dispatchMoveComplete;
+        options.onUpdate = _dispatchTweenUpdate;
+        options.overwrite = 'auto';
 
-        _store.tween = TweenLite.to(self, speed, options);
+        _stopTween();
+        _tween = TweenLite.to(self, speed, options);
       }
     };
 
-    self.holdBorders = function(){
-      if (self.lock) return;
+    self.getAcceleration = function(){
+      var average = {x: 0, y: 0};
 
+      if (self.stack && self.stack.length) {
+        for (var i = 0, _len = self.stack.length; i < _len; i++) {
+          average.x += self.stack[i].x;
+          average.y += self.stack[i].y;
+        }
+
+        average.x /= self.stack.length;
+        average.y /= self.stack.length;
+      }
+
+      var speed = 0 + Math.max(0, .1 * Math.abs((average.x + average.y) / 2));
+
+      return speed;
+    };
+
+    var _holdBorders = function(){
       if (! self.free) {
         if      (self.x < self.borders.x[0]) self.x = self.borders.x[0];
         else if (self.x > self.borders.x[1]) self.x = self.borders.x[1];
@@ -77,125 +94,95 @@ com.firsara.display.MoveClip = (function(){
       }
     };
 
-    self.getAcceleration = function(){
+    var _stopTween = function(){
+      if (_tween) {
+        _tween.kill();
+        _tween = null;
+      }
+    };
+
+    var _dispatchTweenUpdate = function(){
+      _holdBorders();
+
+      if (self.lock) {
+        _stopTween();
+      }
+
+      self.dispatchEvent(MOVE);
+    };
+
+    var _dispatchMoveComplete = function(){
+      self.dispatchEvent(MOVE_COMPLETE);
+    };
+
+    var _startTransform = function(event){
+      _stopTween();
+      if (self.level && self.parent) self.parent.setChildIndex(self, self.parent.getNumChildren() - 1);
+    };
+
+    var _update = function(event){
+      if (self.lock) return;
+
       var average = {x: 0, y: 0};
 
-      if (_store.move && _store.move.length) {
-        for (var i = 0, _len = _store.move.length; i < _len; i++) {
-          average.x += _store.move[i].x;
-          average.y += _store.move[i].y;
+      for (var pointerID in self.fingers) {
+        if (self.fingers[pointerID].start) {
+          average.x += (self.fingers[pointerID].current.x - self.fingers[pointerID].old.x);
+          average.y += (self.fingers[pointerID].current.y - self.fingers[pointerID].old.y);
         }
-
-        average.x /= _store.move.length;
-        average.y /= _store.move.length;
       }
 
-      var speed = 0 + Math.max(0, .1 * Math.abs((average.x + average.y) / 2));
+      average.x /= Math.max(1, self.activeFingers);
+      average.y /= Math.max(1, self.activeFingers);
 
-      return speed;
+      self.x += (average.x * self.friction.move);
+      self.y += (average.y * self.friction.move);
+
+      _holdBorders();
+
+      self.dispatchEvent(MOVE);
     };
 
-    var dispatchUpdate = function(){
-      self.dispatchEvent('update');
-    };
-
-    var dispatchComplete = function(){
-      _store.move = [];
-      self.dispatchEvent('complete');
-    };
-
-    var startDrag = function(e){
-      if (_store.tween) {
-        _store.tween.kill();
-        _store.tween = null;
-      }
-
-      if (self.level && self.parent) self.parent.setChildIndex(self, self.parent.getNumChildren() - 1);
-
-      _store.position.x = e.stageX;
-      _store.position.y = e.stageY;
-    };
-
-    var pressMove = function(e){
+    var _stopTransform = function(event){
       if (self.lock) return;
 
-      var distance = {x: e.stageX - _store.position.x, y: e.stageY - _store.position.y};
-
-      self.x += (distance.x * self.friction.move);
-      self.y += (distance.y * self.friction.move);
-
-      _store.position.x = e.stageX;
-      _store.position.y = e.stageY;
-    };
-
-    // check position update every frame
-    var storeMove = function(){
-      if (self.lock) return;
-
-      var distance = {x: self.x - _store.oldPosition.x, y: self.y - _store.oldPosition.y};
-
-      if (! (distance.x == 0 && distance.y == 0)) {
-        if (self.lock && _store.tween) {
-          _store.tween.kill();
-          _store.tween = null;
-        }
-
-        self.holdBorders();
-        dispatchUpdate();
-      }
-
-      if (_store.move.length > STACK_MOVE) _store.move.shift();
-
-      _store.move.push(distance);
-      _store.oldPosition.x = self.x;
-      _store.oldPosition.y = self.y;
-    };
-
-    var fadeMove = function(){
-      if (! (typeof TweenLite === 'undefined')) {
-        if (self.lock) return;
-
-        if (_store.move.length == 0) {
-          dispatchComplete();
-          return;
-        }
-
-        var speed = 0;
+      if (self.stack.length == 0 || typeof TweenLite === 'undefined') {
+        _dispatchMoveComplete();
+      } else if (! (typeof TweenLite === 'undefined')) {
         var options = {};
         var average = {x: 0, y: 0};
         var newPosition = {x: 0, y: 0};
 
-        for (var i = 0, _len = _store.move.length; i < _len; i++) {
-          average.x += _store.move[i].x;
-          average.y += _store.move[i].y;
+        for (var i = 1, _len = self.stack.length; i < _len; i++) {
+          average.x += (self.stack[i].x - self.stack[i-1].x);
+          average.y += (self.stack[i].y - self.stack[i-1].y);
         }
 
-        average.x = average.x / _store.move.length;
-        average.y = average.y / _store.move.length;
+        average.x = average.x / self.stack.length;
+        average.y = average.y / self.stack.length;
 
-        speed = 0.6 + .01 * Math.abs((average.x + average.y) / 2) * 2 * (self.friction.release + self.friction.release) / 4;
-        newPosition.x = self.x + average.x * Math.max(10, Math.abs(average.x / 10)) * self.friction.release / 2;
-        newPosition.y = self.y + average.y * Math.max(10, Math.abs(average.y / 10)) * self.friction.release / 2;
+        var speed = 0.6 + .01 * Math.abs((average.x + average.y) / 2) * 2 * (self.friction.release + self.friction.release) / 4;
+        options.x = self.x + average.x * Math.max(10, Math.abs(average.x / 10)) * self.friction.release / 2;
+        options.y = self.y + average.y * Math.max(10, Math.abs(average.y / 10)) * self.friction.release / 2;
 
-        if (self.snap && self.snap != 0)
-        {
-          newPosition.x = (Math.round(newPosition.x / self.snap) * self.snap);
-          newPosition.y = (Math.round(newPosition.y / self.snap) * self.snap);
+        if (self.snap && self.snap != 0) {
+          options.x = (Math.round(options.x / self.snap) * self.snap);
+          options.y = (Math.round(options.y / self.snap) * self.snap);
         }
 
-        options.x = newPosition.x;
-        options.y = newPosition.y;
+        // TODO:
+        // check for borders!
+
         options.ease = Cubic.easeOut;
-        options.onComplete = dispatchComplete;
+        options.onComplete = _dispatchMoveComplete;
+        options.onUpdate = _dispatchTweenUpdate;
+        options.overwrite = 'auto';
 
-        _store.tween = TweenLite.to(self, speed, options);
-      } else {
-        dispatchComplete();
+        _stopTween();
+        _tween = TweenLite.to(self, speed, options);
       }
     };
 
-    // call super constructor
-    if (Parent) Parent.call(this);
     // initialize instance
     Init();
   };
